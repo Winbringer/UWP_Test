8d237feb99126dd9173c4ea8c50304b06e8be616
Winbringer <voin1213@yandex.ru>
2016-06-25 21:22:01 +0700

    Добавил еще проектов

--- App1/App3/MainPage.xaml.cs
+++ App1/App3/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace App1
+namespace App3
 {
     /// <summary>
     /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
@@ -25,27 +25,6 @@ namespace App1
         public MainPage()
         {
             this.InitializeComponent();
-            Button button1 = new Button();
-            button1.Content = "Новая кнопка";
-            button1.Width = 120;
-            button1.Height = 40;
-            layoutGrid.Children.Add(button1);
-        }
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            button.Content = "Привет UWP!";
-        }
-    }
-
-    public class Phone
-    {
-        public string Name { get; set; }
-        public int Price { get; set; }
-
-        public override string ToString()
-        {
-            return $"Смартфон {this.Name}; цена: {this.Price}";
         }
     }
 }
