6a91069e8b001be3e9e6dc0aaf1a9deccad2545f
Winbringer <voin1213@yandex.ru>
2016-06-26 18:48:14 +0700

    Еще один урок

---
+++ App1/App5/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App5
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void CalendarView_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            textBlock1.Text = "";
+
+            foreach (var d in calendarView.SelectedDates)
+                textBlock1.Text += d.ToString("dd/MM/yyyy") + "\n";
+
+            // args.AddedDates - новые выделенные даты
+            //args.RemovedDates - даты с которых сняты выделения
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Exit(); // выход из приложения
+        }
+    }
+}
