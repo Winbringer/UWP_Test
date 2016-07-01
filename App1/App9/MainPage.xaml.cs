5df97e83a569ef32752a98f071c564810b542ad2
Winbringer <voin1213@yandex.ru>
2016-07-01 13:37:10 +0700

    Диалоговые окна для открытия и сохранения файлов

---
+++ App1/App9/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App9
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private async void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var savePicker = new FileSavePicker();
+            // место для сохранения по умолчанию
+            savePicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            // устанавливаем типы файлов для сохранения
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".txt" });
+            // устанавливаем имя нового файла по умолчанию
+            savePicker.SuggestedFileName = "New Document";
+            savePicker.CommitButtonText = "Сохранить";
+
+            var new_file = await savePicker.PickSaveFileAsync();
+            if (new_file != null)
+            {
+                await FileIO.WriteTextAsync(new_file, myTextBox.Text);
+            }
+        }
+
+        private async void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            openPicker.CommitButtonText = "Открыть";
+            openPicker.FileTypeFilter.Add(".txt");
+            var file = await openPicker.PickSingleFileAsync();
+
+            if (file != null)
+            {
+                myTextBox.Text = await FileIO.ReadTextAsync(file);
+            }
+        }
+    }
+}
